34eba9bae45cb105c0c70eaadf5a542f3d7fc76a
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2023-11-09 22:26:13 -0300

    Melhorando CardsSections

--- jurisease-web/src/components/ActivitiesBoard/CardsSection/CardsSection.js
+++ jurisease-web/src/components/ActivitiesBoard/CardsSection/CardsSection.js
@@ -11,6 +11,9 @@ function CardsSection({ cardList }) {
     const [position, setPosition] = useState(0);
     const [maxVisibleCards, setMaxVisibleCards] = useState(0);
 
+    const [totalSections, setTotalSections] = useState(1);
+    const [currentSection, setCurrentSection] = useState(1);
+
     useEffect(() => {
         function handleResize() {
             if (window.innerWidth < 500) {
@@ -22,6 +25,9 @@ function CardsSection({ cardList }) {
             } else {
                 setMaxVisibleCards(4);
             }
+
+            const totalSections = Math.ceil(cardList.length / maxVisibleCards);
+            setTotalSections(totalSections);
         }
 
         window.addEventListener('resize', handleResize);
@@ -30,7 +36,7 @@ function CardsSection({ cardList }) {
         return () => {
             window.removeEventListener('resize', handleResize);
         };
-    }, [cardList]);
+    }, [cardList, maxVisibleCards]);
 
     useEffect(() => {
         setCardsVisibles(cardList.slice(position, position + maxVisibleCards));
@@ -44,6 +50,17 @@ function CardsSection({ cardList }) {
             const newPosition = direction === 'left' ? prevPosition - 1 : prevPosition + 1;
             return Math.min(maxPosition, Math.max(minPosition, newPosition));
         });
+
+        // Atualiza a seção atual com base na nova posição
+        const newSection = Math.ceil((position + 1) / maxVisibleCards);
+        setCurrentSection(newSection);
+    }
+
+    function handleIndicatorClick(section) {
+        // Calcula a nova posição com base na seção clicada
+        const newPosition = (section - 1) * maxVisibleCards;
+        setPosition(newPosition);
+        setCurrentSection(section);
     }
 
     if (!cardList || cardList.length === 0) {
@@ -56,17 +73,37 @@ function CardsSection({ cardList }) {
                 <h1>Cards </h1>
                 <MdLibraryAdd className='bt-add-card' />
             </div>
+
             <div className="scroll-container">
                 {cardsVisibles.length > 0 && cardsVisibles.map((card, index) => (
                     <Card data={card} isEditable={true} setOnEditCard={() => { console.log('Edit') }} />
                 ))}
             </div>
+
             <button className="scroll-button left" onClick={() => handleScroll('left')}>
                 <FaAngleLeft className='icon-arrow' />
             </button>
+
             <button className="scroll-button right" onClick={() => handleScroll('right')}>
                 <FaAngleRight className='icon-arrow' />
             </button>
+
+            <div className="section-indicators">
+                {Array.from({ length: !!totalSections ? totalSections : 0 }).map((_, index) => (
+                    <div
+                        key={index}
+                        className={`section-indicator ${currentSection === index + 1 ? 'active' : ''}`}
+                        onClick={() => handleIndicatorClick(index + 1)}
+                    />
+                ))}
+            </div>
+
+            <div className='section-bt-mostrar-todos'>
+                <button className="bt-mostrar-todos" onClick={() => handleScroll('right')}>
+                    Todos os cards
+                </button>
+            </div>
+
         </div>
     );
 }
