5adfc27a9dc73b0dd96614e1d8c7d64ec2730b65
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2024-02-20 08:24:54 -0300

    Disable links to cards templates

--- juris-ease/src/pages/DocsEditor/Template/CardTemplate/CardTemplate.js
+++ juris-ease/src/pages/DocsEditor/Template/CardTemplate/CardTemplate.js
@@ -2,18 +2,13 @@ import './CardTemplate.css';
 import React, { useState } from 'react';
 
 function CardTemplate({ data, onClick }) {
-    const [showFullContent, setShowFullContent] = useState(false);
-    const MAX_CONTENT_LENGTH = 3000;
-
-    const toggleShowContent = () => {
-        setShowFullContent(!showFullContent);
-    };
-
-    const truncatedContent = data.content.length > MAX_CONTENT_LENGTH ?
-        data.content.substring(0, MAX_CONTENT_LENGTH) + "..." : data.content;
-
     const handleCardClick = () => {
         onClick(data);
+    };
+
+    const disableLinks = (html) => {
+        // Substitui todas as tags <a> por <span>
+        return html.replace(/<a\b[^>]*>/gi, '<span>').replace(/<\/a>/gi, '</span>');
     };
 
     if (!data) {
@@ -23,7 +18,7 @@ function CardTemplate({ data, onClick }) {
     return (
         <div className={`CardTemplate ${data.id}`} onClick={handleCardClick}>
             <h1>{data.title}</h1>
-            <div className='template-doc' dangerouslySetInnerHTML={{ __html: showFullContent ? data.content : truncatedContent }} />
+            <div className='template-doc' dangerouslySetInnerHTML={{ __html: disableLinks(data.content) }} />
             <p>{data.rout}</p>
         </div>
     );
