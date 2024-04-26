0104a503605963417c467d54e91a7eca8280be9b
AndrewMarques2018 <andrewmarques2018@gmail.com>
2024-04-26 18:02:49 -0300

    Update ManagmentForms

--- juris-ease/src/pages/GenerateDocks/Forms/Stage_05/ManagmentForms/ManagmentForms.js
+++ juris-ease/src/pages/GenerateDocks/Forms/Stage_05/ManagmentForms/ManagmentForms.js
@@ -1,39 +1,27 @@
+import './ManagmentForms.css'
 import React, { useEffect, useState } from 'react';
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 function ManagmentForms({ form, setForm, templates }) {
 
     const [currentTemplate, setCurrentTemplate] = useState({});
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const handleNext = () => {
-        setCurrentTemplate();
+        if (currentIndex < templates.length - 1) {
+            setCurrentIndex(currentIndex => currentIndex + 1);
+        }
     };
 
     const handleBack = () => {
-        setCurrentTemplate();
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex => currentIndex - 1);
+        }
     };
 
-    const renderForm = () => {
-        return (
-            currentTemplate?.keys && currentTemplate.keys.map(key => {
-                <h2>{currentTemplate.title}</h2>
-                if (['text', 'monetary', 'date', 'number'].includes(key.type)) {
-                    return (
-                        <div className='form-group' key={key.id}>
-                            <label htmlFor={key.id}>{key.id}:</label>
-                            <input
-                                type={key.type === 'monetary' ? 'text' : key.type}
-                                id={key.id}
-                                name={key.id}
-                                value={form[key.id] || ''}
-                                onChange={handleChange}
-                            />
-                        </div>
-                    );
-                }
-                return null;
-            })
-        )
-    };
+    useEffect(() => {
+        setCurrentTemplate(templates[currentIndex] || {});
+    }, [templates, currentIndex]);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -43,19 +31,36 @@ function ManagmentForms({ form, setForm, templates }) {
         }));
     };
 
+    const renderForm = () => {
+        return (
+            currentTemplate?.keys && currentTemplate.keys.map(key => (
+                <div className='form-group' key={key.id}>
+                    <label htmlFor={key.id}>{key.id}:</label>
+                    <input
+                        type={key.type === 'monetary' ? 'text' : key.type}
+                        id={key.id}
+                        name={key.id}
+                        value={form[key.id] || ''}
+                        onChange={handleChange}
+                    />
+                </div>
+            ))
+        );
+    };
+
     return (
         <div className='forms-templates'>
-            {/* <div className='selection-form'>
-                <button className='bt-prev' onAuxClick={handleBack}>
-                    <p>Prev</p>
+            <div className='header'>
+                <button className='next-form' onClick={handleBack}>
+                    <SlArrowLeft />
                 </button>
-
-                <button className='bt-next' onClick={handleNext}>
-                    <p>Next</p>
+                <h2 className='title'>{currentTemplate.title}</h2>
+                <button className='prev-form' onClick={handleNext}>
+                    <SlArrowRight />
                 </button>
             </div>
 
-            {renderForm()} */}
+            {renderForm()}
         </div>
     );
 }
